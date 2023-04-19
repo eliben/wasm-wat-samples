ea5470334fda8e74013fac3c271e57789491480d
Eli Bendersky <eliben@gmail.com>
2023-04-19 06:10:59 -0700

    Update sample and add README

---
+++ wasi-fdwrite/README.md
@@ -0,0 +1,4 @@
+Demonstrates how to use WASI APIs in WAT.
+
+This sample requires a relatively recent version of Node.js (at least 20.0.0),
+in which case no special `--experimental-...` flags are required to run `node`.
--- wasi-fdwrite/test.js
+++ wasi-fdwrite/test.js
@@ -1,14 +1,20 @@
+// Sample of using WASI in WASM and loading it from Node.js
+//
+// Eli Bendersky [https://eli.thegreenplace.net]
+// This code is in the public domain.
 const fs = require("fs");
+
+// This uses experimental APIs described here: https://nodejs.org/api/wasi.html
+// and requires Node 20+
 const { WASI } = require("wasi");
 
 (async () => {
-    const wasi = new WASI();
-    const importObject = {
-        wasi_snapshot_preview1: wasi.wasiImport
-    };
+    const wasi = new WASI({
+        version: "preview1",
+    });
 
     const wasmfile = fs.readFileSync(__dirname + '/write.wasm');
-    let obj = await WebAssembly.instantiate(new Uint8Array(wasmfile), importObject);
+    let obj = await WebAssembly.instantiate(new Uint8Array(wasmfile), wasi.getImportObject());
 
     wasi.start(obj.instance);
 })();
