cfa13b10ee9a18b0cb4627a376348e6a52b41640
Eli Bendersky <eliben@gmail.com>
2023-04-19 07:05:38 -0700

    Test output

--- wasi-fdwrite/test.js
+++ wasi-fdwrite/test.js
@@ -2,6 +2,7 @@
 //
 // Eli Bendersky [https://eli.thegreenplace.net]
 // This code is in the public domain.
+const assert = require('node:assert');
 const fs = require("fs");
 
 // This uses experimental APIs described here: https://nodejs.org/api/wasi.html
@@ -9,12 +10,25 @@ const fs = require("fs");
 const { WASI } = require("wasi");
 
 (async () => {
+    // Create an output file to redirect the WASI output to.
+    const outFilename = __dirname + '/test.out'
+    let outfd = fs.openSync(outFilename, 'w');
+
     const wasi = new WASI({
         version: "preview1",
+        stdout: outfd,
     });
 
     const wasmfile = fs.readFileSync(__dirname + '/write.wasm');
     let obj = await WebAssembly.instantiate(new Uint8Array(wasmfile), wasi.getImportObject());
 
     wasi.start(obj.instance);
+
+    // Now read the output, emit it and test.
+    fs.closeSync(outfd);
+    let wasiout = new TextDecoder('utf8').decode(fs.readFileSync(outFilename));
+    console.log("WASM run successfully with stdout:");
+    console.log(wasiout);
+
+    assert.equal(wasiout, "hello from wat!\n");
 })();
