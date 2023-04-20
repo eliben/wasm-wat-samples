841bcd8c7fa757be183454a64b031b8bd38073a3
Eli Bendersky <eliben@gmail.com>
2023-04-20 11:04:50 -0700

    Make it work and add comments

--- data-crunch/README.md
+++ data-crunch/README.md
@@ -1,9 +1,13 @@
-Each element in the vector has float32 components x,y,z; the layout of a single
-vector element in memory is:
+Demonstrates some "number crunching" in WASM. The code in `vecadd.wat` adds
+together many 3D float32 vectors.
+
+Each vector has float32 components x,y,z; the layout of a single vector in
+memory is:
 
   Offset | Value
   --------------
-   0       x
-   4       y
-   8       z
+     0      x
+     4      y
+     8      z
 
+Vectors are laid out in linear memory back to back, each occupying 12 bytes.
--- data-crunch/test.js
+++ data-crunch/test.js
@@ -1,4 +1,4 @@
-// 
+// Testing of the vector addition function.
 //
 // Eli Bendersky [https://eli.thegreenplace.net]
 // This code is in the public domain.
@@ -8,14 +8,17 @@ const fs = require('fs');
 (async () => {
     const bytes = fs.readFileSync(__dirname + '/vecadd.wasm');
 
-    const memory = new WebAssembly.Memory({initial: 80});
+    const memory = new WebAssembly.Memory({ initial: 80 });
     const mem_f32 = new Float32Array(memory.buffer);
 
     let importObject = {
         env: {
-            log: function (n) {
-                console.log(`log: ${n}`);
+            log_i32: function (n) {
+                console.log(`log i32: ${n}`);
+            },
 
+            log_f32: function (n) {
+                console.log(`log f32: ${n}`);
             },
 
             buffer: memory
@@ -33,12 +36,12 @@ const fs = require('fs');
 
     // Initialize data in memory.
     for (let i = 0; i < count; i++) {
-        let mem_f32_offset = startOffset + i*3;
+        let mem_f32_offset = startOffset + i * 3;
 
         // Populate x,y,z
-        mem_f32[mem_f32_offset] = i*10 + 5;
-        mem_f32[mem_f32_offset+1] = i*11 + 6;
-        mem_f32[mem_f32_offset+2] = i*12 + 7;        
+        mem_f32[mem_f32_offset] = i * 10 + 5;
+        mem_f32[mem_f32_offset + 1] = i * 11 + 6;
+        mem_f32[mem_f32_offset + 2] = i * 12 + 7;
     }
 
     // Call WASM, passing in parameters scaled to its linear memory.
@@ -48,6 +51,18 @@ const fs = require('fs');
     let destX = mem_f32[destOffset];
     let destY = mem_f32[destOffset + 1];
     let destZ = mem_f32[destOffset + 2];
-
     console.log(`result: x=${destX}, y=${destY} z=${destZ}`);
+
+    // Calculate the same sum on the host and compare.
+    let sumX = 0, sumY = 0, sumZ = 0;
+    for (let i = 0; i < count; i++) {
+        let mem_f32_offset = startOffset + i * 3;
+        sumX += mem_f32[mem_f32_offset];
+        sumY += mem_f32[mem_f32_offset + 1];
+        sumZ += mem_f32[mem_f32_offset + 2];
+    }
+
+    assert.equal(destX, sumX);
+    assert.equal(destY, sumY);
+    assert.equal(destZ, sumZ);
 })();
